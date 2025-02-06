2a5941cd03d1ad7c2e377190f45d283e658e29e7
Mehmet Buğra BALCI <bugra.balci@flo.com.tr>
2025-02-07 01:09:51 +0300

    revision

--- Sources/bgrSwift/BGRNetwork.swift
+++ Sources/bgrSwift/BGRNetwork.swift
@@ -13,13 +13,20 @@ public enum RequestMethod: String {
 }
 
 public enum BGRNetworkError: Error {
-    case requestError
-    case responseError
-    case decodingError
+    case requestError(errorMessage: String)
+    case responseError(errorMessage: String)
+    case decodingError(errorMessage: String)
+    
+    var errorMessage: String {
+        switch self {
+        case .requestError(let errorMessage), .responseError(let errorMessage), .decodingError(let errorMessage):
+            return errorMessage
+        }
+    }
 }
 
 protocol BGRNetwork {
-    var decoder: JSONDecoder { get set }
+    var decoder: JSONDecoder { get }
     func request<T: Codable>(model: RequestModel<T>) async throws -> T
 }
 
@@ -32,23 +39,53 @@ public struct NetworkNew: BGRNetwork {
     }()
     
     @discardableResult public func request<T: Codable>(model: RequestModel<T>) async throws -> T {
-        guard let uRLRequest = model.uRLRequest else { throw BGRNetworkError.requestError }
+        guard let uRLRequest = model.uRLRequest else { throw BGRNetworkError.requestError(errorMessage: "Something went wrong! Please check your URL.") }
         
         let (data, response) = try await URLSession.shared.data(for: uRLRequest)
         
-        getRequestInfo(data: data, response: response as? HTTPURLResponse)
+        print(getRequestInfo(data: data, response: response as? HTTPURLResponse))
         
-        guard let httpResponse = response as? HTTPURLResponse, (200..<300).contains(httpResponse.statusCode) else { throw BGRNetworkError.responseError }
+        guard let httpResponse = response as? HTTPURLResponse, (200..<300).contains(httpResponse.statusCode) else { throw BGRNetworkError.responseError(errorMessage: "Something went wrong! Check your status code.")
+        }
         
         do {
             let decodedData = try decoder.decode(model.responseModel.self, from: data)
             return decodedData
         } catch {
-            throw BGRNetworkError.decodingError
+            throw BGRNetworkError.decodingError(errorMessage: "Something went wrong! Please check your response model.")
         }
     }
     
-    private func getRequestInfo(data: Data, response: HTTPURLResponse?) -> String {
+    public func request<T: Codable>(model: RequestModel<T>, completion: @escaping (Result<T, BGRNetworkError>) -> Void) async {
+        guard let uRLRequest = model.uRLRequest else {
+            completion(.failure(.requestError(errorMessage: "Something went wrong! Please check your URL.")))
+            return
+        }
+        
+        do {
+            let (data, response) = try await URLSession.shared.data(for: uRLRequest)
+            
+            getRequestInfo(data: data, response: response as? HTTPURLResponse)
+            
+            guard let httpResponse = response as? HTTPURLResponse, (200..<300).contains(httpResponse.statusCode) else {
+                completion(.failure(.responseError(errorMessage: "Something went wrong! Please check the response.")))
+                return
+            }
+            
+            do {
+                let decodedData = try decoder.decode(model.responseModel.self, from: data)
+                completion(.success(decodedData))
+            } catch {
+                completion(.failure(.decodingError(errorMessage: "Something went wrong! Please compare your response model with the response.")))
+            }
+        } catch {
+            completion(.failure(.requestError(errorMessage: "Something went wrong! Please check your connection.")))
+        }
+    }
+}
+
+extension NetworkNew {
+    @discardableResult private func getRequestInfo(data: Data, response: HTTPURLResponse?) -> String {
         var requestInfo = "*** REQUEST INFO ***"
         requestInfo.append("\n********************")
         
@@ -60,8 +97,10 @@ public struct NetworkNew: BGRNetwork {
 
         requestInfo.append("\n* Response Data ↓")
         requestInfo.append("\n________________⌋")
-        requestInfo.append("\n \(String(data: data, encoding: .utf8) ?? "-")")
+        requestInfo.append("\n \(String(data: data, encoding: .utf8) ?? "There is no data!")")
         requestInfo.append("\n------------------")
+        
+        print(requestInfo)
         
         return requestInfo
     }
